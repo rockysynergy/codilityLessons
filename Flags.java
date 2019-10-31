c106d01e38f1d38ce9a53d05766eb678446e120e
Rocky Ou <rockysynergy@qq.com>
2019-10-31 14:47:37 +0800

    Now use Hashmap to make Flags.java runing fast, still failed one performance testing thus only got 93%

--- Flags.java
+++ Flags.java
@@ -1,12 +1,15 @@
-import java.util.ArrayList;
+import java.util.HashMap;
+
 
 class Flags {
-    private ArrayList<Integer> peaks = new ArrayList<Integer>();
+    private HashMap<Integer, Boolean> peaks = new HashMap<>();
+    private int first = -1;
 
     public int solution(int[] A) {
         findPeaks(A);
         int L = A.length;
         if (peaks.size() < 1) return 0;
+
 
         int lo = 0;
         int hi = L - 1;
@@ -22,17 +25,18 @@ class Flags {
         int L = A.length;
         for (int i = 1; i < L-1; i++) {
             if (A[i-1] < A[i] && A[i] > A[i+1]) {
-                peaks.add(i);
+                peaks.put(i, true);
+                if (first == -1) first = i;
             }
         }
     }
 
     private boolean check(int x, int[] A) {
-        int pos = peaks.get(0);
+        int pos = first;
         int flags = x;
         int L = A.length;
         while (pos < L && flags > 0) {
-            if (peaks.contains(pos)) {
+            if (peaks.containsKey(pos)) {
                 flags--;
                 pos += x;
             } else {
@@ -45,7 +49,17 @@ class Flags {
 
     public static void main(String[] args) {
         Flags f = new Flags();
-        int[] A = new int[] {5};
+        int[] A = new int[] {0, 0, 1, 0, 1, 0, 1};
         System.out.println(f.solution(A));
+
+        // HashMap<Integer, Boolean> B= new HashMap<>();
+        // B.put(1, true);
+        // boolean C;
+        // for (int i = 0; i < 4; i++) {
+        //     C = B.get(i);
+        //     if (C) {
+        //         System.out.println("True for " + i);
+        //     }
+        // }
     }
 }
